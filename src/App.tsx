41037cf9ee531a8d9da6ddc297ee5b91c315cf70
VanoKi <kirsvano@gmail.com>
2026-04-13 15:13:36 +0300

    feat: add first responce

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { useEffect } from "react";
 import './App.css';
+import {instance} from "@/shared/api/api-instance.ts";
 
 function App() {
 
   useEffect(() => {
-    console.log('useEffect');
+    instance.get('todo-list').then(response => {
+      console.log(response)
+    });
   }, []);
 
   return (
--- src/shared/api/api-instance.ts
+++ src/shared/api/api-instance.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-axios.create({
+export const instance =axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: {
     API_KEY: import.meta.env.VITE_API_KEY,
-    AUTH_TOKEN: import.meta.env.VITE_AUTH_TOKEN,
   },
 })
