c078ad9bbbf1e529ee8bf3285f35bf88b41f7074
VanoKi <kirsvano@gmail.com>
2026-06-10 18:17:20 +0300

    fix: types

---
+++ src/api/tasks.types.ts
@@ -0,0 +1,13 @@
+export type taskType = {
+    description: string
+    title: string
+    completed: boolean
+    status: number
+    priority: number
+    startDate: string
+    deadline: string
+    id: string
+    todoListId: string
+    order: number
+    addedDate: string
+};
